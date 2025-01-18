d0890494539b2b696cc522bbad9b120d906ffc43
MrAll <81728811+ALMAZIK17364@users.noreply.github.com>
2025-01-18 12:18:54 +0800

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Главная страница"
+
+
+@app.get("/user/admin")
+async def admin_page():
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def join_account(user_id: int) -> dict:
+    return {"Вы вошли как пользователь № ": user_id}
+
+@app.get("/user")
+async def user_info(username: str = "Steve", age: int = 42 ) -> dict:
+    return {"Информация о пользователе Имя ": username, "Возраст ": age}
