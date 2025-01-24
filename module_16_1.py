b2b37ef384b857c29230e2e83c37795244f4e3d5
MrAll <81728811+ALMAZIK17364@users.noreply.github.com>
2025-01-24 13:34:58 +0800

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -12,9 +12,9 @@ async def admin_page():
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def join_account(user_id: int) -> dict:
+async def join_account(user_id: str) -> dict:
     return {"Вы вошли как пользователь № ": user_id}
 
 @app.get("/user")
-async def user_info(username: str = "Steve", age: int = 42 ) -> dict:
+async def user_info(username: str = "Steve", age: str = 42) -> dict:
     return {"Информация о пользователе Имя ": username, "Возраст ": age}
